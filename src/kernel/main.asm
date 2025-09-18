0b112bcd2242d2ea5ee05937419699f9297b0f2b
THMapnea <thmapneadesign@gmail.com>
2025-09-18 22:53:49 +0200

    made custom welcome message

--- src/kernel/main.asm
+++ src/kernel/main.asm
@@ -41,14 +41,27 @@ main:
     mov ss, ax
     mov sp, 0x7C00
 
-    mov si, msg_hello
+    ;printing the welcome
+    mov si, msg_q1
+    call puts
+    mov si, msg_q2
+    call puts
+    mov si, msg_q3
+    call puts
+    mov si, msg_q4
+    call puts
+    mov si, msg_q5
     call puts
 
 .halt
     jmp .halt
 
 
-msg_hello db 'Welcome to QvantumFishOS', ENDL, 0
+msg_q1 db ' ___                   _                   _____ _     _      ___  ____', ENDL, 0
+msg_q2 db '/ _ \__   ____ _ _ __ | |_ _   _ _ __ ___ |  ___(_)___| |__  / _ \/ ___| ', ENDL, 0
+msg_q3 db '| | | \ \ / / _  |  _ \| __| | | |  _   _ \| |_  | / __|  _ \| | | \___ \ ', ENDL, 0
+msg_q4 db '| |_| |\ V / (_| | | | | |_| |_| | | | | | |  _| | \__ \ | | | |_| |___) |', ENDL, 0
+msg_q5 db ' \__\_\ \_/ \__ _|_| |_|\__|\__ _|_| |_| |_|_|   |_|___/_| |_|\___/|____/ ', ENDL, 0
 
 times 510-($-$$) db 0
 
